9af07dcb587a7fd7b58b8047ebfa422b388cd5ef
Victor Eduardo de Sousa Silva <veduardosousa@apple.com>
2021-12-08 15:54:24 -0300

    update ShortUrl title when calling the update_title! method

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,15 +1,20 @@
 class ShortUrl < ApplicationRecord
-
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
   validates :full_url, presence: true, uniqueness: true
 
   validate :validate_full_url
 
+  before_validation :generate_short_code, on: :create
+
   def short_code
   end
 
   def update_title!
+    require 'open-uri'
+
+    title = open(full_url).read.scan(/<title>(.*?)<\/title>/)
+    update(title: title.flatten.first)
   end
 
   private
@@ -20,6 +25,7 @@ class ShortUrl < ApplicationRecord
     if !uri.is_a?(URI::HTTP) || uri.host.nil?
       errors.add(:full_url, 'is not a valid url')
     end
+  rescue URI::InvalidURIError
+    errors.add(:full_url, 'is not a valid url')
   end
-
 end
