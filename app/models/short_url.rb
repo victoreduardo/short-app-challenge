c68210546d4e5ed667a9a8a2f3554dc68472ef4a
Victor Eduardo de Sousa Silva <veduardosousa@apple.com>
2021-12-08 15:53:05 -0300

    adds validate for full_url attr on the ShortUrl model

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,6 +2,8 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates :full_url, presence: true, uniqueness: true
+
   validate :validate_full_url
 
   def short_code
@@ -13,6 +15,11 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    uri = URI.parse(full_url)
+
+    if !uri.is_a?(URI::HTTP) || uri.host.nil?
+      errors.add(:full_url, 'is not a valid url')
+    end
   end
 
 end
